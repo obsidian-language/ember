d61050230d9287af4e0136f85d4332e728e5bced
PeterGriffinSr <codezz-ops@protonmail.com>
2025-03-04 20:22:10 -0500

    made life easier

--- src/include/ui.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void print_help_menu(unsigned short height, unsigned short width);
-void draw_border(int height, int width, const char *title);
-void draw_main_interface(unsigned short height, int selected_index, const char *version, const char *tags);
-int draw_ui();
-
-#endif
