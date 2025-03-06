50935a8898cd72f26c646f906872012d17d7165f
PeterGriffinSr <codezz-ops@protonmail.com>
2025-03-06 17:32:00 -0500

    added help

--- src/cli.py
+++ src/cli.py
@@ -2,24 +2,42 @@ import sys
 from utils import print_help
 from installer import install_tool, remove_tool
 from ui import draw_ui
+import pkg_resources
 import curses
 
+def get_version():
+    return pkg_resources.get_distribution('ember').version
+
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2 or sys.argv[1] in ["-h", "--help"]:
         print_help()
-        sys.exit(1)
+        sys.exit(0)
     
     command = sys.argv[1]
-    if command == "install" and len(sys.argv) >= 4:
+    
+    if command == "install":
+        if len(sys.argv) != 4 or sys.argv[2] in ["-h", "--help"]:
+            print_help(command)
+            sys.exit(1)
         install_tool(sys.argv[2], sys.argv[3])
-    elif command == "rm" and len(sys.argv) >= 4:
+    
+    elif command == "rm":
+        if len(sys.argv) != 4 or sys.argv[2] in ["-h", "--help"]:
+            print_help(command)
+            sys.exit(1)
         remove_tool(sys.argv[2], sys.argv[3])
-    elif command in ["rm", "install", "cli"]:
-        print_help(command)
-    elif command == "tui":
-        curses.wrapper(draw_ui)
+    
+    elif command in ["tui", "version"]:
+        if command == "tui":
+            curses.wrapper(draw_ui)
+        elif command == "version":
+            version = get_version()
+            print(f"ember version {version}")
+        else:
+            print_help(command)
+    
     else:
-        print("Unknown command. Use -h for help.")
+        print("Unknown command. Use -h or --help for help.")
 
 if __name__ == "__main__":
     main()
